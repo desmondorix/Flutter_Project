a4c5d7b09e46fcbfc861e794c59581b386be4948
samuelnababan <samuelnababan20@student.esaunggul.ac.id>
2023-12-11 15:49:59 +0700

    MARK.01

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marbel/pages/main_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -101,7 +102,7 @@ class _LoginPageState extends State<LoginPage> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => const LoginPage(),
+                    builder: (context) => const MainPage(),
                   ),
                 );
               },
