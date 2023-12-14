9b2c1a2e0f756bc92ba07d1bfd7717cacd3801b0
azureusho <willyueu0211@gmail.com>
2023-12-14 14:08:46 +0700

    Willy Push UI Belajar dan Kategori

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:marbel/pages/home_page.dart';
+import 'package:marbel/pages/senjata.dart';
+
+import 'Widget/reuseable_card.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const Senjata(),
     );
   }
 }
