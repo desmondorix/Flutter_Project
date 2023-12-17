c9b6c6d18739c044cfd07d5927b2e3c0e2b198fc
jovancamorris <you@example.comjovancamoris@gmail.com>
2023-12-17 22:28:55 +0700

    final

--- lib/widget/answerContain.dart
+++ lib/widget/answerContain.dart
@@ -1,79 +1,50 @@
 import 'package:flutter/material.dart';
-class OpsiJawaban extends StatefulWidget {
-  final String jawaban1;
-  final String jawaban2;
-  final String jawaban3;
-  final String jawaban4;
-  final Function(int) onSelect;
 
-  const OpsiJawaban({
-    Key? key,
-    required this.jawaban1,
-    required this.jawaban2,
-    required this.jawaban3,
-    required this.jawaban4,
-    required this.onSelect,
-  }) : super(key: key);
+class ContainerOption extends StatelessWidget {
+  final String text;
+  final String color;
+  final String? selectedOption;
+  final VoidCallback onTap;
 
-  @override
-  State<OpsiJawaban> createState() => _OpsiJawabanState();
-}
-
-class _OpsiJawabanState extends State<OpsiJawaban> {
-  int selectedOptionIndex = -1; // Initialize with an invalid index
+  ContainerOption(this.text, this.color, this.selectedOption, this.onTap);
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    double containerWidth = size.width * 0.7; // Adjust the width as needed
-
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          buildOptionButton(widget.jawaban1, containerWidth, 0),
-          SizedBox(height: 16.0),
-          buildOptionButton(widget.jawaban2, containerWidth, 1),
-          SizedBox(height: 16.0),
-          buildOptionButton(widget.jawaban3, containerWidth, 2),
-          SizedBox(height: 16.0),
-          buildOptionButton(widget.jawaban4, containerWidth, 3),
-        ],
-      ),
-    );
-  }
-
-  Widget buildOptionButton(String text, double containerWidth, int index) {
-    bool isSelected = selectedOptionIndex == index;
-
-    return GestureDetector(
-      onTap: () {
-        // Handle the option selection
-        setState(() {
-          selectedOptionIndex = index;
-        });
-        widget.onSelect(index); // Notify the parent widget about the selected option
-        print('Selected option: $text');
-      },
-      child: Container(
-        width: containerWidth,
-        height: 55.0, // Adjust the height as needed
-        padding: EdgeInsets.all(12.0),
-        decoration: BoxDecoration(
-          color: isSelected ? Color(0xFFB64D3A) : Color(0xFFCF9C6F),
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18.0,
+    return Padding(
+      padding: EdgeInsets.all(8.0), // Add padding around each container
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          width: 200.0, // Set a fixed width for all containers
+          height: 50.0, // Adjust the height as needed
+          padding: EdgeInsets.all(12.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            color: selectedOption == text ? Color(0xFFB64D3A) : getColor(color),
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 18, color: Colors.white),
             ),
           ),
         ),
       ),
     );
+  }
+
+  Color getColor(String? color) {
+    switch (color) {
+      case 'Red':
+        return Color(0xFFCF9C6F);
+      case 'Green':
+        return Colors.green;
+      case 'Blue':
+        return Colors.blue;
+      case 'Purple':
+        return Colors.purple;
+      default:
+        return Colors.grey;
+    }
   }
 }
--- lib/widget/questionContain.dart
+++ lib/widget/questionContain.dart
@@ -1,81 +1,86 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class DestinationCard extends StatefulWidget {
-  final String question;
-  final String imagePath;
 
-  const DestinationCard({
-    Key? key,
-    required this.question,
-    required this.imagePath,
-  }) : super(key: key);
-
-  @override
-  State<DestinationCard> createState() => _DestinationCardState();
-}
-
-class _DestinationCardState extends State<DestinationCard> {
-  late String question;
-  late String imagePath;
-  bool like = false;
-
-  @override
-
-
-  @override
-  void initState() {
-    super.initState();
-    question = widget.question;
-    imagePath = widget.imagePath;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Card(
-      elevation: 5,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: GestureDetector(
-        child: Container(
-          width: size.width,
-          height: size.height / 2.7,
-          decoration: BoxDecoration(
-            color: Color(0xFFCF9C6F),
-            borderRadius: const BorderRadius.all(Radius.circular(30)),
-          ),
-          child: Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.all(Radius.circular(30)),
-                child: Image(
-                  fit: BoxFit.fill,
-                  image: NetworkImage(imagePath),
-                  width: size.height,
-                  height: 220,
-                ),
+Widget getQuestionContainer(int questionNumber, BuildContext context) {
+  Size size = MediaQuery.of(context).size;
+  return Card(
+    elevation: 5,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(30),
+    ),
+    child: GestureDetector(
+      child: Container(
+        width: size.width,
+        height: size.height / 2.8,
+        decoration: BoxDecoration(
+          color: Color(0xFFCF9C6F),
+          borderRadius: const BorderRadius.all(Radius.circular(30)),
+        ),
+        child: Stack(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(30)),
+              child: Image(
+                fit: BoxFit.cover,
+                image: NetworkImage(getQuestionImageUrl(questionNumber)),
+                width: size.height,
+                height: 220,
               ),
-              Positioned(
-                bottom: 15,
-                left: 23,
-                right: 23,
-                child: Text(
-                  question,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
-                  softWrap: true,
+            ),
+            Positioned(
+              bottom: 8,
+              left: 23,
+              right: 23,
+              child: Text(
+                getQuestionText(questionNumber),
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
+                textAlign: TextAlign.center,
+                softWrap: true,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
-    );
+    ),
+  );
+}
+String getQuestionImageUrl(int questionNumber) {
+  // Provide image URLs based on the question number
+  switch (questionNumber) {
+    case 1:
+      return 'https://asset.kompas.com/crops/-tukq-UsIymUQXKSNIR6jGI23fA=/0x0:1000x667/750x500/data/photo/2022/12/31/63afd57be0e64.jpg';
+    case 2:
+      return 'https://asset.kompas.com/crops/-tukq-UsIymUQXKSNIR6jGI23fA=/0x0:1000x667/750x500/data/photo/2022/12/31/63afd57be0e64.jpg';
+    case 3:
+      return 'https://asset.kompas.com/crops/-tukq-UsIymUQXKSNIR6jGI23fA=/0x0:1000x667/750x500/data/photo/2022/12/31/63afd57be0e64.jpg';
+    case 4:
+      return 'https://asset.kompas.com/crops/-tukq-UsIymUQXKSNIR6jGI23fA=/0x0:1000x667/750x500/data/photo/2022/12/31/63afd57be0e64.jpg';
+    case 5:
+      return 'https://asset.kompas.com/crops/-tukq-UsIymUQXKSNIR6jGI23fA=/0x0:1000x667/750x500/data/photo/2022/12/31/63afd57be0e64.jpg';
+  // ... Add URLs for other questions as needed
+    default:
+      return '';
   }
 }
+
+String getQuestionText(int questionNumber) {
+  switch (questionNumber) {
+    case 1:
+      return 'What is the capital of France?';
+    case 2:
+      return 'Which planet is known as the Red Planet?';
+    case 3:
+      return 'What is the largest mammal on Earth?';
+    case 4:
+      return 'In which year did Christopher Columbus reach the Americas?';
+    case 5:
+      return 'What is the main ingredient in guacamole?';
+    default:
+      return '';
+  }
+}
+
