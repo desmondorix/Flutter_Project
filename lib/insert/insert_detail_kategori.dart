5ca8f741eb6a8873a33999de4ad2503070bbb09a
cintia <cintiauml@gmail.com>
2023-12-21 19:17:02 +0700

    test

---
+++ lib/insert/insert_detail_kategori.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+import 'dart:io';
+
+import '../main.dart';
+import '../view/admin_detail_kategori.dart';
+import '../view/kategori.dart';
+
+
+class insertdetailk extends StatefulWidget {
+  String id_kategori;
+  String nama_kategori;
+
+  insertdetailk(this.id_kategori, this.nama_kategori);
+
+  @override
+  State<insertdetailk> createState() => _insertdetailkState();
+}
+
+class _insertdetailkState extends State<insertdetailk> {
+  TextEditingController id_kategori=TextEditingController();
+  TextEditingController nama_kategori=TextEditingController();
+  TextEditingController caption=TextEditingController();
+  TextEditingController asal_daerah=TextEditingController();
+  TextEditingController detail_daerah=TextEditingController();
+
+  File? imagepath;
+  String? imagename;
+  String? imagedata;
+
+  ImagePicker imagePicker = new ImagePicker();
+
+  List userdata = [];
+
+
+  Future<void> uploadImage() async{
+    if(caption.text !=""){
+      try{
+        String uri = "http://10.0.2.2:8080/study_flutter/detail_kategori/insert_detail_kategori.php";
+        var res=await http.post(Uri.parse(uri), body: {
+          "caption":caption.text,
+          "data":imagedata,
+          "name":imagename,
+          "asal_daerah":asal_daerah.text,
+          "id_kategori":id_kategori.text,
+          "detail_daerah":detail_daerah.text,
+        });
+        var response = jsonDecode(res.body);
+        if(response["success"]=="true"){
+          print("Record Inserted");
+          caption.text = "";
+          asal_daerah.text = "";
+          detail_daerah.text = "";
+          imagedata;
+          imagename;
+        } else {
+          print("some issue");
+        }
+      } catch(e){
+        print(e);
+      }
+    } else {
+      print("please fill all fields");
+    }
+  }
+
+  Future<void> getImage() async{
+    var getimage=await imagePicker.pickImage(source:ImageSource.gallery);
+    setState(() {
+      imagepath = File(getimage!.path);
+      imagename=getimage.path.split('/').last;
+      imagedata=base64Encode(imagepath!.readAsBytesSync());
+      print(imagepath);
+      print(imagedata);
+      print(imagename);
+    });
+  }
+
+  @override
+  void initState() {
+
+    id_kategori.text=widget.id_kategori;
+    nama_kategori.text=widget.nama_kategori;
+    super.initState();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Insert Detail Kategori'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.home, color: Colors.black, size: 35,),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context)=>MyApp())
+              );
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+          children: <Widget> [
+            Column(
+              children: [
+                SizedBox(height: 20,),
+                Container(
+                  margin: EdgeInsets.only(right: 30, left: 30),
+                  child: TextFormField(
+                    readOnly: true,
+                    controller: id_kategori,
+                    decoration: InputDecoration(border: OutlineInputBorder(),
+                        label: Text('ID Kategori')),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  margin: EdgeInsets.only(right: 30, left: 30),
+                  child: TextFormField(
+                    readOnly: true,
+                    controller: nama_kategori,
+                    decoration: InputDecoration(border: OutlineInputBorder(),
+                        label: Text('Nama Kategori')),
+                  ),
+                ),
+
+                SizedBox(height: 20,),
+                Container(
+                  margin: EdgeInsets.only(right: 30, left: 30),
+                  child: TextFormField(
+                    controller: caption,
+                    decoration: InputDecoration(border: OutlineInputBorder(),
+                        label: Text('Enter nama contoh (Kerak Telor)')),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  margin: EdgeInsets.only(right: 30, left: 30),
+                  child: TextFormField(
+                    controller: asal_daerah,
+                    decoration: InputDecoration(border: OutlineInputBorder(),
+                        label: Text('Enter Asal Daerah')),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  margin: EdgeInsets.only(right: 30, left: 30,),
+                  child: TextFormField(
+                    controller: detail_daerah,
+                    decoration: InputDecoration(border: OutlineInputBorder(),
+                        label: Text('Enter Detail')),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                Container(
+                  alignment: Alignment.center,
+                  width: 350,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    border: Border.all(),
+                  ),
+                  child: imagepath != null
+                      ? Image.file(imagepath!)
+                      : Text('Image Not Chose Yet', style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.w500),),
+                ),
+                ElevatedButton(onPressed: (){
+                  getImage();
+                }, child: Text('Chose Image')),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    uploadImage();
+                  });
+                }, child: Text('Upload')),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Builder(
+                    builder: (context){
+                      return ElevatedButton(
+                          onPressed: (){
+
+                          },
+                          child: Text('View data')
+                      );
+                    },
+                  ),
+                ),
+              ],
+            )
+          ]
+      ),
+    );
+  }
+}
---
+++ lib/view/kategori.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
+import 'package:flutter_background/flutter_background.dart';
+
+import 'detail_kategori.dart';
+
+class Kategori extends StatefulWidget {
+  const Kategori({super.key});
+
+  @override
+  State<Kategori> createState() => _KategoriState();
+}
+
+class _KategoriState extends State<Kategori> {
+  List userdata = [];
+  /*Future<void> grecord(String id) async {
+    String uri = "http://10.0.2.2:8080/study_flutter/view_data_makanan.php?id=$id";
+    try {
+      var response = await http.get(Uri.parse(uri));
+      setState(() {
+        userdata = jsonDecode(response.body);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }*/
+  Future<void> getrecord() async {
+    String uri = "http://10.0.2.2:8080/study_flutter/kategori/view_data_kategori.php";
+    try {
+      var response = await http.get(Uri.parse(uri));
+      setState(() {
+        userdata = jsonDecode(response.body);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+  @override
+  void initState() {
+    getrecord();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        shape: ContinuousRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(50),
+          ),
+        ),
+        title: Text('Kategori Budaya',
+          style: const TextStyle(fontWeight: FontWeight.bold),
+        ),
+        backgroundColor: Colors.grey[400],
+        foregroundColor: Colors.black,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.logout, color: Colors.black, size: 35,),
+            onPressed: () {
+            },
+          ),
+        ],
+      ),
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/tari/bg_batik.png"), fit: BoxFit.fill),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 15.0),
+          child: GridView.builder(
+              gridDelegate:
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemCount: userdata.length,
+              itemBuilder: (BuildContext context, index) {
+                return SizedBox(
+                    width: size.width,
+                    child: Card(
+                      elevation: 10,
+                      color: Color(0xFFCF9C6F),
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(20))),
+                      margin: EdgeInsets.all(10),
+                      child: Column(
+                        children: [
+                          Material(
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                            child: InkWell(
+                              onTap: (){
+                                getrecord();
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context)=>Makanan(
+                                        userdata[index]["id_kategori"].toString(),
+                                        userdata[index]["nama_kategori"]
+                                    ))
+                                );
+                              },
+                              child: ClipRRect(
+                                borderRadius: const BorderRadius.all(
+                                  Radius.circular(20),
+                                ),
+                                child: Image.network("http://10.0.2.2:8080/study_flutter/kategori/" +
+                                    userdata[index]["foto_kategori"],
+                                  width: size.width,
+                                  height: 130,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            alignment: Alignment.topCenter,
+                            margin: const EdgeInsets.only(top: 8),
+                            child: Text(userdata[index]["nama_kategori"],
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+
+                );
+              }),
+        ),
+      ),
+    );
+  }
+}
